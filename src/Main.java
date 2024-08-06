80b28d3860039ba1d6964f05393e14ae5fe72ad6
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-08-06 10:48:47 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+class main {
+    static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("Índices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("Nenhuma soma encontrada.");
+        }
+    }
+}
